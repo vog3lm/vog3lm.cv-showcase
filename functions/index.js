c0550dacf4890c69d523539d3c49ed91259971ba
mv <mv-161648@hs-weingarten.de>
2018-03-07 21:09:46 +0100

    forward to presenter page after remote login

--- functions/index.js
+++ functions/index.js
@@ -12,7 +12,6 @@ const crypto = require('crypto');
 const path = require('path');
 
 
-
 /* vog3lm smart cv private content backend api */
 const validateFirebaseIdToken = (req, res, next) => {
 	if ((!req.headers.authorization || !req.headers.authorization.startsWith('CONTENT_ID_TOKEN::')) && !req.cookies.__session) {
@@ -55,11 +54,14 @@ const validateFirebaseIdToken = (req, res, next) => {
 	}
 	return;
 };
-const mvc = express();
-mvc.use(cors);
-mvc.use(cookieParser);
-mvc.use(validateFirebaseIdToken);
-mvc.get('/', (req, res) => {
+
+
+
+const test = express();
+test.use(cors);
+test.use(cookieParser);
+test.use(validateFirebaseIdToken);
+test.get('/', (req, res) => {
 
 	/* funzt sicher aber iwi umstaendlich */
 	var hat = {'title':'vog3lm.cv','manifest':'./manifest.json','fav':'https://vog3lm-0x1.firebaseapp.com/images/favicon.coyote.poly_white.b.png'}
@@ -71,11 +73,15 @@ mvc.get('/', (req, res) => {
 			 ,'./js/animate.dotNet.js']
 	var css = ['./css/font-awesome.min.css'
 			  ,'./css/landing.white.css']
-//	var methods = {get:()=>{return 'Name Vom User oder sonst was...'}} // bad bad bad...aber geht
-//	return res.render("landing",{'hat':hat,'css':css,'js':js,'cre':cre});
+	//	var methods = {get:()=>{return 'Name Vom User oder sonst was...'}} // bad bad bad...aber geht
+	//	return res.render("landing",{'hat':hat,'css':css,'js':js,'cre':cre});
 	res.send(`Hello ${req.user.email} on ${req.user.aud}`);
 });
-exports.mvc = functions.https.onRequest(mvc);
+exports.test = functions.https.onRequest(test);
+
+
+
+
 
 
 
@@ -92,104 +98,141 @@ const getQrRequestTokenFromHeader = (req, res, next) => {
 		req.qrToken = req.headers.authorization.split('[qR1D]::[')[1]
 	}
 	return next();
-}
-*//* 	vog3lm smart cv mobile qr code login backend api */
+}*/
+
+
+/* 	vog3lm smart cv mobile qr code login backend api */
 var db = {'PXHTajCVg':{'mail':'adm1n@smart.cv.de','pass':'44920420$Dollar'  ,'tag':''}
-		 ,'1w9cBiU7Q':{'mail':'card@smart.cv.de' ,'pass':'c4rd@$m4r1.cv.d3' ,'tag':'2018FEB'}
-		 ,'PT5VTsF8R':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2018FEB'}
+		 ,'1w9cBiU7Q':{'mail':'card@smart.cv.de' ,'pass':'c4rd@$m4r1.cv.d3' ,'tag':'2018,märz'}
+		 /* flyer qr ID token 12-tupel-k15 *//*	ordered, p:previous, n:next	*/
+		 ,'PT5VTsF8c':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2015,nov'}
+		 ,'PT5VTsF8b':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2015,nov'}
+		 ,'PT5VTsF8a':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2015,nov'}
+		 ,'PT5VTsF8Z':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2015,nov'}
+		 ,'PT5VTsF8Y':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2015,nov'}
+		 ,'PT5VTsF8X':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2015,nov'}
+		 ,'PT5VTsF8W':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2015,nov'}
+		 ,'PT5VTsF8V':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2015,nov'}
+		 ,'PT5VTsF8U':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2015,nov'}
+		 ,'PT5VTsF8T':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2015,nov'}
+		 ,'PT5VTsF8S':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2015,nov'}
+		 ,'PT5VTsF8R':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2015,nov'}
+		 /* flyer qr ID token 9-tupel-k18 *//*	ordered, p:previous, n:next	*/
+		 ,'aEC2Mj4fN':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2018,märz'}
+		 ,'gJfrKris9':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2018,märz'}
+		 ,'lNY4hRkMv':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2018,märz'}
+		 ,'ZZvqAmOsx':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2018,märz'}
+		 ,'DEPl3yjiS':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2018,märz'}
+		 ,'lvhPjXCTi':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2018,märz'}
+		 ,'hN3EptFwt':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2018,märz'}
+		 ,'VA0850MmS':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2018,märz'}
+		 ,'uiLT6kFBG':{'mail':'flyer@smart.cv.de','pass':'fly3r@$m4r1.cv.d3','tag':'2018,märz'}
 		 /* custom cv users */
 		 ,'6PW2gsYzD':{'mail':'unset','pass':'unset','tag':''}
 		 ,'O2FNkkqqE':{'mail':'unset','pass':'unset','tag':''}}
-
-const qr = express();
-qr.set('view engine', 'pug')
-qr.set('views', path.join(__dirname,'templates'));
-qr.use(express.static(path.join(__dirname, "static")));
-qr.use(cors);
-/*	/qr/?qR1D=12345]	*//*
-	qR client -> https://us-central1-vog3lm-0x1.cloudfunctions.net/qr/?qR1D=PXHTajCVgPkXKQgn54r]
-	qR client <- page
-	qR client -> login request
-	qR client <- login grant
-	qR client -> redirect https://vog3lm-0x1.firebaseapp.com/
-*//*
- 	FUNKTIONIERT NICHT
-	LOGGT SICH MIT DER https://us-central1-vog3lm-0x1.cloudfunctions.net/ DOMAIN EIN 
- 	SESSION GEHT BEIM REDIRECT AUF https://vog3lm-0x1.firebaseapp.com/ VERLOREN (DOMAIN DIVERGENCE)
- 	GANZE SEITE BAUEN DAUERT VOLL LANG
-*/
-qr.get('/', (req, res) => {
-	if (!req.query.qR1D){
-		console.error('No qR ID token was passed as a content ID token in the request parameters.',
-			'Make sure you authorize your request by providing the following parameter pair: qR1D=token');
-		return res.status(404).send('Ooops');
-	}
-
-	req.qrToken = req.query.qR1D
-	req.qrCreds = db[req.qrToken];
-	console.log('qr ID token: ',req.qrToken)
-	if(!req.qrCreds){
-		console.error('No qR ID credentials were found in database for the ID token in the request parameters.'
-					 ,'The current ID token could be expired. Try to request a new one.'
-					 ,'DB::'+JSON.stringify(db),'qR1D::'+req.qrToken);
-		return res.status(404).send('Ooops');
-	}
-	console.log('qR ID credentials: ',req.qrCreds)
-	var js = ['https://www.gstatic.com/firebasejs/4.9.1/firebase-app.js'
-			 ,'https://www.gstatic.com/firebasejs/4.9.1/firebase-auth.js'
-			 ,'https://code.jquery.com/jquery-2.2.4.min.js'
-			 ,'./js/backend.js'
-			 ,'./js/animate.dotNet.js'];
-	var css = ['./css/font-awesome.min.css'
-			  ,'./css/landing.white.css'];
-
-	var redirectUrl = 'https://vog3lm-0x1.firebaseapp.com';
-	// route redirect with no history
-	var hat = {'title':'vog3lm.cv.redirect','fav':'https://vog3lm-0x1.firebaseapp.com/images/favicon.coyote.poly_white.b.png'};
-	var cre = {'mail':'test@user.de','pass':'44920420','apiKey':'AIzaSyDmXSkwOam-aQ37z8-3d5aH-X257lIVS34','authDomain':'vog3lm-0x1.firebaseapp.com','projectId':'vog3lm-0x1'};
-	var route = {'error':redirectUrl+'/404','qr':redirectUrl};
-	return res.status(200).render("qr",{'hat':hat,'cre':cre,'route':route});
-});
-exports.test = functions.https.onRequest(qr);
-
-/*	/qr/?qR1D=12345]	*//*
-	qR client -> https://vog3lm-0x1.firebaseapp.com -> https://us-central1-vog3lm-0x1.cloudfunctions.net/test/?qR1D=PXHTajCVgPkXKQgn54r]
+/*	/qr/?qR1D=id	*//*	TODO TODO MOVE TO STORAGE FILE
+	qR client -> https://vog3lm-0x1.firebaseapp.com -> https://us-central1-vog3lm-0x1.cloudfunctions.net/test/?qR1D=id&v=view
 	qR client    									<- login page
-	qR client -> login domain https://vog3lm-0x1.firebaseapp.com
-	qR client -> redirect https://vog3lm-0x1.firebaseapp.com
-	qR client <- logged in https://vog3lm-0x1.firebaseapp.com
+	qR client -> google auth domain
+	qR client -> redirect content page
+	qR client <- logged content view
 */
 exports.qr = functions.https.onRequest((req,res)=>{
-	var qrRedirect = 'https://vog3lm-0x1.firebaseapp.com'
+	var qrDomain = 'https://vog3lm-0x1.firebaseapp.com'
 	var qrPage = ''
 		+ '<!DOCTYPE html><html lang="de"><head><title>vog3lm.cv.redirect</title>'
-		+ '<link rel="shortcut icon" href="'+qrRedirect+'/images/favicon.coyote.poly_white.b.png" type="image/x-icon">'
-		+ '<link rel="icon" ref="'+qrRedirect+'/images/favicon.coyote.poly_white.b.png" type="image/x-icon">';
+		+ '<link rel="shortcut icon" href="'+qrDomain+'/images/favicon.coyote.poly_white.b.png" type="image/x-icon">'
+		+ '<link rel="icon" ref="'+qrDomain+'/images/favicon.coyote.poly_white.b.png" type="image/x-icon">[INJ:SCRIPT]'
+		+ '</head><body></body></html>';
 	if (!req.query.qR1D){
 		console.error('No qR ID token was passed as a content ID token in the request parameters.',
 			'Make sure you authorize your request by providing the following parameter pair: qR1D=token');
-		return res.status(200).send(qrPage + '</script><script type="text/javascript">window.location.replace("'+qrRedirect+'/404")</script>')
+		return res.status(200).send(qrPage.replace('[INJ:SCRIPT]','</script><script type="text/javascript">window.location.replace("'+qrDomain+'/404")</script>'));
 	}
-	req.qrToken = req.query.qR1D
-	req.qrCreds = db[req.qrToken];
-	console.log('qr ID token: ',req.qrToken)
-	if(!req.qrCreds){
-		console.error('No qR ID credentials were found in database for the ID token in the request parameters.'
-					 ,'The current ID token could be expired. Try to request a new one.'
-					 ,'qR1D::'+req.qrToken);
-		return res.status(200).send(qrPage + '</script><script type="text/javascript">window.location.replace("'+qrRedirect+'/404")</script>')
+	if(!db.hasOwnProperty(req.query.qR1D)){
+		console.error('No user was found for this qR ID token.', 'Make sure you pass existing qR ID tokens as parameter pair: qR1D=token');
+		return res.redirect(404).end();
 	}
-	console.log('qR ID credentials: ',req.qrCreds);
-	qrPage = qrPage
-	+ '<script type="text/javascript" src="https://www.gstatic.com/firebasejs/4.9.1/firebase-app.js">'
-	+ '</script><script type="text/javascript" src="https://www.gstatic.com/firebasejs/4.9.1/firebase-auth.js"></script><script type="text/javascript">'
-	+ 'firebase.initializeApp({apiKey:"AIzaSyDmXSkwOam-aQ37z8-3d5aH-X257lIVS34",authDomain: "vog3lm-0x1.firebaseapp.com",projectId: "vog3lm-0x1"});const a = firebase.auth();'
-	+ 'a.onAuthStateChanged(function(u){if(u){window.user = u;window.location.replace("'+qrRedirect+'");}else{window.location.replace("'+qrRedirect+'/404");}});'
-	+ 'a.signInWithEmailAndPassword("'+req.qrCreds.mail+'","'+req.qrCreds.pass+'").catch(function(error){window.location.replace("'+qrRedirect+'/404");});'
-	+ '</script></head><body></body></html>';
-	return res.status(200).send(qrPage);
+	var qrCreds = db[req.query.qR1D]
+	var qrRedirect = qrDomain+'/reader'+'?q='+req.query.qR1D;
+	return res.status(200).send(qrPage.replace('[INJ:SCRIPT]',
+		  '<script type="text/javascript" src="https://www.gstatic.com/firebasejs/4.9.1/firebase-app.js"></script>'
+		+ '<script type="text/javascript" src="https://www.gstatic.com/firebasejs/4.9.1/firebase-auth.js">'
+		+ '</script><script type="text/javascript">'
+			+ 'firebase.initializeApp({apiKey:"AIzaSyDmXSkwOam-aQ37z8-3d5aH-X257lIVS34",authDomain:"vog3lm-0x1.firebaseapp.com",projectId:"vog3lm-0x1"});const a=firebase.auth();'
+		//	+ 'a.onAuthStateChanged(function(u){if(u){window.user = u;console.log("'+qrRedirect+'");}else{console.error("'+qrDomain+'/404");}});'
+		//	+ 'a.signInWithEmailAndPassword("'+qrCreds.mail+'","'+qrCreds.pass+'").catch(function(error){console.error("'+qrDomain+'/404",error);});'
+			+ 'a.onAuthStateChanged(function(u){if(u){window.user = u;window.location.replace("'+qrRedirect+'");}else{window.location.replace("'+qrDomain+'/404");}});'
+			+ 'a.signInWithEmailAndPassword("'+qrCreds.mail+'","'+qrCreds.pass+'").catch(function(error){window.location.replace("'+qrDomain+'/404");});'
+		+ '</script>'));
+});
+/*	/mvp/?q=view	*//*	TODO TODO MOVE TO STORAGE FILE
+	qR client -> https://vog3lm-0x1.firebaseapp.com -> https://us-central1-vog3lm-0x1.cloudfunctions.net/mvc/?q=view
+	qR client    									<- content
+*/
+var vb = {'PXHTajCVg':{'mail':'adm1n@smart.cv.de','pass':'44920420$Dollar'  ,'tag':''}
+		 ,'1w9cBiU7Q':{'mail':'card@smart.cv.de' ,'pass':'c4rd@$m4r1.cv.d3' ,'tag':'2018,landing'}
+		 /* flyer qr ID token 12-tupel-k15 *//*	ordered, p:previous, n:next	*/
+		 ,'PT5VTsF8c':{'p':'PT5VTsF8b','n':'PT5VTsF8R','tag':'1995,gs,grundschule'}	
+		 ,'PT5VTsF8b':{'p':'PT5VTsF8a','n':'PT5VTsF8c','tag':'2001,rs,realschule,abschluss,mittlere reife'}	
+		 ,'PT5VTsF8a':{'p':'PT5VTsF8Z','n':'PT5VTsF8b','tag':'2004,bank,ksw,berufschule,ausbildung,abschluss,bwl'}
+		 ,'PT5VTsF8Z':{'p':'PT5VTsF8Y','n':'PT5VTsF8a','tag':'2005,2006,zivi,zivildienst,bad wurzach,rheumaklinik'}
+		 ,'PT5VTsF8Y':{'p':'PT5VTsF8X','n':'PT5VTsF8Z','tag':'2009,abi,abitur,matura,abschluss,bwl,vwl,wirtschaft'}
+		 ,'PT5VTsF8X':{'p':'PT5VTsF8W','n':'PT5VTsF8Y','tag':'2010,konstanz,universität,mathematik'}
+		 ,'PT5VTsF8W':{'p':'PT5VTsF8V','n':'PT5VTsF8X','tag':'2013,bad wurzach,rheumaklinik,bwl'}
+		 ,'PT5VTsF8V':{'p':'PT5VTsF8U','n':'PT5VTsF8W','tag':'2013,weingarten,hochschule,fh,tutor,mathematik'}
+		 ,'PT5VTsF8U':{'p':'PT5VTsF8T','n':'PT5VTsF8V','tag':'2014,weingarten,hochschule,fh,bachelor,abschluss,informatik'}
+		 ,'PT5VTsF8T':{'p':'PT5VTsF8S','n':'PT5VTsF8U','tag':'2014,fn,friedrichshafen,zf,trainee,praktikum,abschluss,bachelor,informatik'}
+		 ,'PT5VTsF8S':{'p':'PT5VTsF8R','n':'PT5VTsF8T','tag':'2015,weingarten,schülerhilfe,lehrer,tutor,pädagogik'}
+		 ,'PT5VTsF8R':{'p':'PT5VTsF8c','n':'PT5VTsF8S','tag':'2015,about,landing'}
+		 /* flyer qr ID token 9-tupel-k18 *//*	ordered, p:previous, n:next	*/
+		 ,'aEC2Mj4fN':{'p':'gJfrKris9','n':'O2FNkkqqE','tag':'2001,gs,grundschule,rs,realschule,abschluss'}
+		 ,'gJfrKris9':{'p':'lNY4hRkMv','n':'aEC2Mj4fN','tag':'2004,sparkasse,bankkaufmann,ausbildung,ihk,berufsschule,ausbildung,abschluss,bwl,vwl,wirtschaft'}
+		 ,'lNY4hRkMv':{'p':'ZZvqAmOsx','n':'gJfrKris9','tag':'2006,zivildinest,rheumaklinik,soziales'}
+		 ,'ZZvqAmOsx':{'p':'DEPl3yjiS','n':'lNY4hRkMv','tag':'2009,abitur,gymnasium,bwl,vwl,wirtschaft,pädagogik,informatik,rheumaklinik'}
+		 ,'DEPl3yjiS':{'p':'lvhPjXCTi','n':'ZZvqAmOsx','tag':'2013,studium,mathematik,informatik,konstanz,weingarten,universität,hochschule,pädagogik,rheumaklinik'}
+		 ,'lvhPjXCTi':{'p':'hN3EptFwt','n':'DEPl3yjiS','tag':'2014,studium,hochschule,it,abschluss,bachelor,zf,weingarten'}
+		 ,'hN3EptFwt':{'p':'VA0850MmS','n':'lvhPjXCTi','tag':'2018,studium,hochschule,pädagogik,bwl,vwl,wirtschaft,informatik,schülerhilfe'}
+		 ,'VA0850MmS':{'p':'uiLT6kFBG','n':'hN3EptFwt','tag':'2018,skills,informatik,programmieren'}
+		 ,'uiLT6kFBG':{'p':'aEC2Mj4fN','n':'VA0850MmS','tag':'2018,about,landing'}}
+
+var dto = {"cols":[],"recs":[],"meta":{"state":"success","type":"dto","id":"","tag":""}};
+var vto = {"view":"unset","meta":{"state":"success","type":"vto","id":"test","tag":"test"}};
+
+exports.mvp = functions.https.onRequest((req,res)=>{
+	if(!req.query.q){
+		console.error('No query parameter q was passed as a content ID token in the request parameters.',
+			'Make sure you authorize your request by providing the following parameter pair: q=view');
+		return res.redirect(404);
+	}
+	if(!vb.hasOwnProperty(req.query.q)){
+		console.error('No view was found for this query parameter.', 'Make sure you pass existing view tokens as parameter pair: q=view');
+		return res.redirect(404);
+	}
+	var testData = vb[req.query.q];
+
+	var data = JSON.parse(JSON.stringify(vto));
+	data.view = testData;
+	return res.status(200).send(data);
 });
 
 
+exports.mvc = functions.https.onRequest((req,res)=>{
+	if(!req.query.q){
+		console.error('No query parameter q was passed as a content ID token in the request parameters.',
+			'Make sure you authorize your request by providing the following parameter pair: q=view');
+		return res.redirect(404);
+	}
+	if(!vb.hasOwnProperty(req.query.q)){
+		console.error('No view was found for this query parameter.',
+			'Make sure you pass existing view tokens as parameter pair: q=view');
+		return res.redirect(404);
+	}
+	var testPage = '<div>'+'[INJ:CONTENT]'+'</div>';
+	testPage = testPage.replace('[INJ:CONTENT]',JSON.stringify(vb[req.query.q]));
 
+	var data = JSON.parse(JSON.stringify(vto));
+	data.view = testPage
+	return res.status(200).send(data);
+});
 
--- pages/js/backend.js
+++ pages/js/backend.js
@@ -13,37 +13,54 @@ function F1rebas3Storage4p1Operator(firebase){
 
 function F1rebas3Auth4p1Operator(firebase){
 	var base = firebase.auth();
-	var token = 'unset';
-	this.getToken = function(){return token;}	// mandatory
-	this.setToken = function(t){token = t;}
-	this.create = function(){
-		if(window.user){
-			conslole.log(window.user)
+	var dispatcher = null;
+	var events = {
+		'log-in':(user) => {	
+			base.setPersistence(args.scope).then(() => {
+				/* Note: Firebase Auth web sessions are single host origin 
+						 and will be persisted for a single domain only.  */
+				base.signInWithEmailAndPassword(user.mail,user.pass).catch((error) => {
+				  	consoles.error('login error',error);
+					args.user = 'unset';
+					args.token = 'unset';
+					$('body').trigger('logged-out',{'call':'logged-out','id':'auth-log-in'})
+				});
+			}).catch((error) => {console.error(error);});}
+	    ,'log-out':(data) => {base.signOut().catch((error) => {console.error('login error',error)});}
+	  /*,'log-in-custom':(token) => {base.signInWithCustomToken(token).catch(function(error){console.log(error)});}*/
+	};
+	var args = {'events':events,'user':'unset','token':'unset','firebase':'unset','scope':firebase.auth.Auth.Persistence.LOCAL};
+	this.getToken = function(){return args.token;}	// mandatory
+	this.decorate = function(opts){
+		for(var key in opts) {
+		    if(args.hasOwnProperty(key)) {
+		    	if('events' !== key){
+		    		args[key] = opts[key];
+		    	}
+		    }
 		}
 		return this;
 	}
+	this.create = function(){
+		if('unset' !== args.firebase){base = firebase.auth();}
+		if('unset' !== args.user){
+			conslole.log(window.user);
+		}
 
-//	base.signInWithCustomToken(token).catch(function(error) {
-//		// Handle Errors here.
-//		var errorCode = error.code;
-//		var errorMessage = error.message;
-//		// ...
-//	});
+		switch(args.scope) {
+			case 'none':args.scope = firebase.auth.Auth.Persistence.NONE; break;
+			case 'session':args.scope = firebase.auth.Auth.Persistence.SESSION; break;
+			default:args.scope = firebase.auth.Auth.Persistence.LOCAL; break;
+		}
+		dispatcher = new V13wEv3ntD1spatch3r({'events':Object.keys(events),'issues':Object.values(events)}).onRegister();
 
-	this.logIn = function(user) {
-		base.signInWithEmailAndPassword(user.mail,user.pass).catch(function(error) {
-		  	console.error('login error',error)
-			token = 'unset';
-			$('body').trigger('logged-out',{'call':'logged-out','id':'auth-log-in'})
-		});
+		return this;
 	}
-	this.logOut = function() {
-		base.signOut().catch(function(error) {
-		  	console.error('login error',error)
-		});
-	}
+
+
+	/* move to content request js... */
 	this.reqHeader = function(url,type="GET"){
-		if(token !== 'unset'){
+		if('unset' !== args.token){
 			console.log('Sending request to', url, 'with ID token in Authorization header.');
 			var req = new XMLHttpRequest();
 			req.onload = function() {
@@ -55,7 +72,7 @@ function F1rebas3Auth4p1Operator(firebase){
 				//this.responseContainer.innerText = 'There was an error';
 			};
 			req.open(type,url,true);
-			req.setRequestHeader('Authorization', 'CONTENT_ID_TOKEN::'+token);
+			req.setRequestHeader('Authorization', 'CONTENT_ID_TOKEN::'+args.token);
 			req.send();
 		} else {
 			console.error('No Content ID Token Found!')
@@ -63,10 +80,10 @@ function F1rebas3Auth4p1Operator(firebase){
 		// firebase.auth().currentUser.getIdToken().then(function(token){/* call token each request */});
 	}
 	this.reqCookie = function(url,type="GET"){
-		if(token !== 'unset'){
+		if('unset' !== args.token){
 			// set the __session cookie
-			document.cookie = '__content_id_token=' + token + ';max-age=3600';
-			console.log('Sending request to', url, 'with ID token in __content_id_token cookie.');
+			document.cookie = '__session=' + args.token + ';max-age=3600';
+			console.log('Sending request to', url, 'with ID token in __session cookie.');
 			var req = new XMLHttpRequest();
 			req.onload = function() {
 				console.log(req.responseText)
@@ -81,36 +98,17 @@ function F1rebas3Auth4p1Operator(firebase){
 		}
 		// firebase.auth().currentUser.getIdToken().then(function(token){/* call token each request */});
 	}
+	/* move to content request js... */
 
-	function setUserPersistence(user,refresh){
-		base.setPersistence(firebase.auth.Auth.Persistence.SESSION).then(() => {
-			// firebase.auth.Auth.Persistence.LOCAL
-			// firebase.auth.Auth.Persistence.SESSION
-			// firebase.auth.Auth.Persistence.NONE
-			//
-			// Note that Firebase Auth web sessions are single host origin and will be persisted for a single domain only.
-			// 
-			return firebase.auth().signInWithEmailAndPassword(email, password);
-		})
-		.catch((error) => {
-			// Handle Errors here.
-			var errorCode = error.code;
-			var errorMessage = error.message;
-		});
-	}
-	function getUserToken(user,refresh){
-		// var user = firebase.auth().currentUser
-		user.getIdToken(/* forceRefresh */refresh).then(function(userToken){
-			token = userToken
-			$('body').trigger('got-token',{'call':'got-token','id':'','token':token})
-		}).catch(function(error){
-			console.error(error);
-		});
-	}
 
-	base.onAuthStateChanged(function(u){
+	base.onAuthStateChanged((u) => {
+	//	console.info(u,'User authentication state change!')
 		if(u){
-			getUserToken(u,true);		// force token refresh!
+			args.user = u;
+			u.getIdToken(/* forceRefresh */true).then((userToken) => {
+				args.token = userToken;
+				$('body').trigger('got-token',{'call':'got-token','id':'','token':args.token})
+			}).catch((error) => {console.error(error);});
 			$('body').trigger('logged-in',{
 				 'call':'logged-in'
 				,'id':'auth-state-listener'
@@ -120,7 +118,8 @@ function F1rebas3Auth4p1Operator(firebase){
 				,'anonym':u.isAnonymous
 			});
 		} else {
-			token = 'unset';
+			args.user = 'unset';
+			args.token = 'unset';
 			$('body').trigger('logged-out',{'call':'logged-out','id':'auth-state-listener'})
 		}
 	});
@@ -128,28 +127,66 @@ function F1rebas3Auth4p1Operator(firebase){
 
 
 function St1l34p1Operator(){
-	this.remove = function(filename) {
-		var linkNode = document.querySelector('link[href*="'+filename+'"]');
-		linkNode.parentNode.removeChild(linkNode)
-	}
-	this.add = function(filename){
-	    var fileref=document.createElement("link")
+	this.remove = function(filename){wrapRemove(filename);}
+	this.add = function(filename){wrapAdd(filename);}
+
+	var args = {'design':'white','designs':['white','dark']
+			   ,'path':'css','url':'design','format':'css'}
+
+    function wrapAdd(filename){
+		var fileref=document.createElement("link")
 	    fileref.setAttribute("rel", "stylesheet")
 	    fileref.setAttribute("type", "text/css")
 	    fileref.setAttribute("href",filename)
-	    if (typeof fileref!="undefined")
+	    if("undefined" !== typeof fileref){
 	        document.getElementsByTagName("head")[0].appendChild(fileref)
-	}
+	    } else {
+	    	console.error('design not found',filename)
+	    }
+    }
+    function wrapRemove(filename){
+		var linkNode = document.querySelector('link[href*="'+filename+'"]');
+		if(linkNode){
+			linkNode.parentNode.removeChild(linkNode);
+		} else {
+			console.error('design not found',filename)
+		}
+    }
+
+	var events = {
+		'design-call':(data) => {
+			wrapRemove(args.path+'/'+args.url+'.'+args.design+'.'+args.format);
+			args.design = data.design
+			wrapAdd(args.path+'/'+args.url+'.'+args.design+'.'+args.format);
+		}
+		,'design-toggle':(data) => {
+			wrapRemove(args.path+'/'+args.url+'.'+args.design+'.'+args.format);
+			if('white' == args.design){args.design = 'dark'}
+			else if('dark' == args.design){args.design = 'white'}
+			wrapAdd(args.path+'/'+args.url+'.'+args.design+'.'+args.format);
+		}
+	    ,'design-iterate':(data) => {
+			wrapRemove(args.path+'/'+args.url+'.'+args.design+'.'+args.format);
+			var index = args.designs.indexOf(args.design);
+			index++;
+			if(index < args.designs.length){args.design = args.designs[index];}
+			else{args.design = args.designs[0];}
+			wrapAdd(args.path+'/'+args.url+'.'+args.design+'.'+args.format);
+	    }
+	};
+	new V13wEv3ntD1spatch3r({'events':Object.keys(events),'issues':Object.values(events)}).onRegister();
+
 }
 
 
 function V13wEv3ntD1spatch3r(holder){
-    var events = holder.events
-    var issues = holder.issues
+    var events = holder.events;
+    var issues = holder.issues;
     this.onRegister = function(){
         for (var i=0; i<events.length; i++) {
             $('body').on(events[i],selfDispatch);
         };
+        return this;
     }
     this.onUnleash = function(){
         $("a").each(function(){
@@ -157,7 +194,7 @@ function V13wEv3ntD1spatch3r(holder){
             if(element.attr('call')){
                 element.on('click',function(){
                     var tmp = $(this)
-                    $('body').trigger(tmp.attr('call'),[{call:tmp.attr('call'),id:tmp.attr('id')}]);
+                    $('body').trigger(tmp.attr('call'),[{call:element.attr('call'),id:element.attr('id'),url:tmp.attr('url')}]);
                 });
             }
         });
@@ -166,10 +203,11 @@ function V13wEv3ntD1spatch3r(holder){
                 e.preventDefault();
                 var element = $(this)               
                 if(element.attr('call')){
-                    $('body').trigger(element.attr('call'),[{call:element.attr('call'),id:element.attr('id')}]);
+                    $('body').trigger(element.attr('call'),[{call:element.attr('call'),id:element.attr('id'),url:element.attr('url')}]);
                 }
             }
         });
+        return this;
     }
     function selfDispatch(evt,data){
         try {
